72c29bd48e65ec86d1ffff38b18c4c6fc9241232
bonnifer <bemappleton@hotmail.com>
2016-06-12 18:59:00 +0100

    Added data from the database to the html template

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import ListItem
 
 def shopping_list(request):
-    return render(request, 'shoppinglist/shopping_list.html', {})
+    list_items = ListItem.objects.filter(displayed=True)
+    return render(request, 'shoppinglist/shopping_list.html', {'list_items': list_items})
