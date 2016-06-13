9e78e5246d10fcd2c3d46ff8ac8a1dbb34562425
bonnifer <bemappleton@hotmail.com>
2016-06-13 21:17:59 +0100

    Adding 'add' functionality, bug fixes to blog

--- shoppinglist/forms.py
+++ shoppinglist/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import ListItem
 
-class ShoppingForm(forms.MetaForm):
+class ShoppingForm(forms.ModelForm):
     class Meta:
         model = ListItem
         fields = ('name', 'section',)
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from .models import ListItem
+from .forms import ShoppingForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 def shopping_list(request):
+    logger.error('I am rendering the page')
+    if request.method == "POST":
+        add_item(request)
     list_items = ListItem.objects.filter(displayed=True)
     return render(request, 'shoppinglist/shopping_list.html', {'list_items': list_items})
 
 def add_item(request):
-    if request.method == "POST":
-        form = ShoppingForm(request.POST)
-        if form.is_valid():
-            item = form.save(commit=False)
-            item.displayed = True
-            item.save()
+    form = ShoppingForm(request.POST)
+    print ('I am adding an item')
+    logger.error(request.POST)
+    if form.is_valid():
+        logger.error('form is valid')
+        item = form.save(commit=False)
+        item.displayed = True
+        item.save()
     else:
-        form = ShoppingForm()
-    return render(request, 'shoppinglist/shopping_list.html', {'list_items': list_items})
+        logger.error('not valid')
+        logger.error(form.errors)
