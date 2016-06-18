dfb16c03c0e0fcaa9022f66c86870134312c6ce8
bonnifer <bemappleton@hotmail.com>
2016-06-18 19:26:32 +0100

    Adding delete and checkbox saving functionality

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -8,7 +8,17 @@ logger = logging.getLogger(__name__)
 def shopping_list(request):
     logger.error('I am rendering the page')
     if request.method == "POST":
-        add_item(request)
+        logger.error(request.POST)
+        if 'name' in request.POST:
+            add_item(request)
+        elif 'delete' in request.POST:
+            logger.error("I am deleting an item")
+            delete_item(request)
+        elif 'save' in request.POST:
+            logger.error("I am dealing with the checkboxes")
+            checkboxes(request)
+        else:
+            logger.error("Error!")
     list_items = ListItem.objects.filter(displayed=True)
     return render(request, 'shoppinglist/shopping_list.html', {'list_items': list_items})
 
@@ -24,3 +34,20 @@ def add_item(request):
     else:
         logger.error('not valid')
         logger.error(form.errors)
+
+def delete_item(request):
+    #Get value of delete from request.POST
+    item_id = request.POST['delete']
+    logger.error('id is ' + item_id)
+    #Update the object with that id to make displayed = False and checked = False
+    ListItem.objects.filter(id=item_id).update(displayed=False, checked=False)
+
+def checkboxes(request):
+    #Get value of checkboxes from request.POST
+    item_ids_list = request.POST.getlist('list-item')
+    #Update all displayed objects to be checked=False
+    ListItem.objects.filter(displayed=True).update(checked=False)
+    #Update displayed objects with values as ids to be checked=True
+    for item_id in item_ids_list:
+        logger.error('id: ' + item_id)
+        ListItem.objects.filter(id=item_id).update(checked=True)
